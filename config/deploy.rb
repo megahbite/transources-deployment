985e825c1536d3d9b1a85df5b410f22bc8bb351e
Megan Bowra-Dean <megan@rabidtech.co.nz>
2014-10-10 17:51:12 +1300

    Remove bin linking.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :linked_files, %w{config/database.yml config/local_env.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
